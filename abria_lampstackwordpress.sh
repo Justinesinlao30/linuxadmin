4cfe7017086f6fc63b47c73a26432234d0ec6003
Justinesinlao30 <83442004+Justinesinlao30@users.noreply.github.com>
2022-06-09 17:14:45 +0800

    Update abria_lampstackwordpress.sh

--- abria_lampstackwordpress.sh
+++ abria_lampstackwordpress.sh
@@ -21,14 +21,6 @@ systemctl enable httpd
 echo "Restarting httpd service"
 systemctl restart httpd
 
-# add all changes to the git server
-git add -A
-
-# commit changes added to the git and add a message
-git commit -m "added abria_apachewebpage.sh"
-
-# upload the script to github
-git push origin main
 
 # start php apache using mysql
 echo "Starting PHP Apache"
@@ -41,15 +33,6 @@ yum install -y php-fpm
 # go to var www html and php phpinfo 
 cd /var/www/html/
 echo '<? php phpinfo(); ?>' > index.php
-
-# add all changes to the git server
-git add -A
-
-# commit changes added to the git and add a message
-git commit -m "added abria_phpapache_phpfpm.sh"
-
-# upload the script to github
-git push origin main
 
 # install rsync
 echo "Installing Rsync"
@@ -116,7 +99,7 @@ FLUSH PRIVILEGES;" | mysql -u root -p$passw
 echo "Installing wordpress"
 yum install -y php-gd
 
-# restart Apache httpd service 
+# restart apache httpd service 
 echo "Restarting Apache"
 service httpd restart
 
@@ -133,15 +116,6 @@ cd /opt/
 wget http://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
 
-# add all changes to the git server
-git add -A
-
-# commit changes added to the git and add a message
-git commit -m "added abria_mysqlmariadb.sh"
-
-# upload the script to github
-git push origin main
-
 # install a http rpms remirepo and remi release-7
 echo "Installing HTTP"
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
@@ -154,7 +128,7 @@ yum install -y yum-utils
 echo "Updating Config Manager & Enable Remi PHP56" 
 yum-config-manager --enable remi-php56
 
-# update a PHP
+# update a php
 echo "Updating php"
 yum install -y php php-mysql
 
@@ -162,11 +136,19 @@ yum install -y php php-mysql
 echo "Installing PHP"
 yum install -y php php-mcrypt php-cli php-gd php-curl php-ldap php-zip php-fileinfo
 
-# to check rsync avp wordpress to web server
+# rsync -avP wordpress
 rsync -avP wordpress/ /var/www/html/
+
+# mkdir
 mkdir /var/www/html/wp-content/uploads
+
+# chown -r apache apache
 chown -R apache:apache /var/www/html/*
+
+# html
 cd /var/www/html
+
+# copy wp-config php
 cp wp-config-sample.php wp-config.php
 
 # restart httpd service
@@ -177,7 +159,7 @@ systemctl restart httpd.service
 git add -A
 
 # commit changes added to the git and add a message
-git commit -m "added abria_assignement81.sh"
+git commit -m "added lampstackwordpress.sh"
 
 # upload the script to github
 git push origin main
